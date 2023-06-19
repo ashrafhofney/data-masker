aa6b192a06576ba09b8ec68580ab342afa2e0b8f
ashrafhofney <am.hofney@gmail.com>
2023-06-19 16:28:50 +0300

    support cjs and ecm

---
+++ README.md
@@ -0,0 +1,3 @@
+# modern-npm-package
+
+An npm package for masking sensitive data, built for both the ECMAScript Module format (i.e. ESM or ES Module) and CommonJS Module format (CJS). It can be used in Node.js and browser applications.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,56 @@
-import { DataMasker } from "./data-masker";
+import { snakeCase } from "snake-case";
+import { MaskConfigInput } from "./mask-config.input";
 
-module.exports = DataMasker;
+export default class DataMasker {
+  private data: string;
+  private mask: string;
+  private keys: string[];
+  private secrets: string[];
+
+  constructor(config: MaskConfigInput) {
+    this.data = config?.data || "";
+    this.mask = config?.mask || "******";
+    this.keys = [
+      ...config?.keys,
+      ...config?.keys?.map((key: string) => snakeCase(key)),
+    ];
+    this.secrets = config?.values || [];
+  }
+
+  run() {
+    this.keys.forEach((key: string) => {
+      this.replaceJsonKey(key);
+      this.replaceStringKey(key);
+    });
+
+    this.secrets.forEach((secret: string) => {
+      this.data = this.data.replace(secret, this.mask);
+    });
+    return this.data;
+  }
+
+  private replaceJsonKey(key: string) {
+    const stringQuote = "('|\")?";
+    const skipSpaces = "\\s*";
+    const matchEverything = "(.*)";
+    const regex = new RegExp(
+      "(" +
+        stringQuote +
+        key +
+        stringQuote +
+        skipSpaces +
+        ":" +
+        skipSpaces +
+        stringQuote +
+        matchEverything +
+        stringQuote +
+        ")"
+    );
+    this.data = this.data.replace(regex, '"' + key + '":"' + this.mask + '"');
+  }
+
+  private replaceStringKey(key: string) {
+    const regex = new RegExp(`${key}=[^&]+`, "g");
+    this.data = this.data.replace(regex, `${key}=${this.mask}&`);
+  }
+}
